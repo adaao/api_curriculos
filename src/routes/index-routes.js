e76348d7c4adab9ff04317621dfa356bd5e12ea1
adaao <armascarelli@gmail.com>
2020-09-23 08:47:20 -0300

    index route

--- src/routes/index-routes.js
+++ src/routes/index-routes.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+   res.status(200).send({
+      title: 'Curriculo API',
+      version: '0.1'
+   });
+});
+
 module.exports = router;
